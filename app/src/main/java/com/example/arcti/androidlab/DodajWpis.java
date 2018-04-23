cbaf6e95bc88c5e3cf1095c9194a655e8ba7bd0c
arctic95 <38659879+arctic95@users.noreply.github.com>
2018-04-23 17:03:19 +0200

    listview

--- app/src/main/java/com/example/arcti/androidlab/DodajWpis.java
+++ app/src/main/java/com/example/arcti/androidlab/DodajWpis.java
@@ -1,7 +1,11 @@
 package com.example.arcti.androidlab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DodajWpis extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class DodajWpis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis);
+
+        final Button przycisk = (Button) findViewById(R.id.button);
+
+        przycisk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wyslij(v);
+            }
+        });
+    }
+
+    public void wyslij (View view) {
+        EditText kontrolka =
+                (EditText) findViewById(R.id.editText);
+        String pole =
+                kontrolka.getText().toString();
+        Intent intencja = new Intent();
+        intencja.putExtra("wpis", pole);
+        setResult(RESULT_OK, intencja);
+        finish();
     }
 }
